a90e7f24bda59b29e9fd2de2fe32df97cc55d4b0
vishikant <63965405+vishikant@users.noreply.github.com>
2023-12-06 11:08:04 +0530

    Add files via upload

---
+++ Prog Assignment1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a Python program to print &quot;Hello Python&quot;?
+print("Hello Python")
+
+
+# In[2]:
+
+
+#2. Write a Python program to do arithmetical operations addition and division.?
+a=10
+b=5
+print(a+b) #addition
+print(a/b) #divison
+
+
+# In[3]:
+
+
+#Write a Python program to find the area of a triangle?
+base=10
+height=2
+area=(1/2)*base*height
+print(area)
+
+
+# In[6]:
+
+
+#Write a Python program to swap two variables?
+a=10
+b=20
+print("values before swap",a,b)
+temp=a
+a=b
+b=temp
+print("values after swap",a,b)
+
+
+# In[8]:
+
+
+#5. Write a Python program to generate a random number?
+import random
+import numpy as np
+a=np.random.randint(80)
+print(a)
+
+
+# In[ ]:
+
+
+
+
